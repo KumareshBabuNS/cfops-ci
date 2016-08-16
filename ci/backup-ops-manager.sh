3a1edbe95b7b07f74eee42048b1dd1d5b45d802f
patrickcrocker <patrick.crocker@gmail.com>
2016-08-16 08:46:20 -0500

    initial commit

---
+++ ci/backup-ops-manager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LOG_LEVEL=debug cfops backup \
+  -opsmanagerhost $OPSMANAGER_HOST Ops Manager VM hostname] \
+  -adminuser $ADMIN_USER \
+  -adminpass $ADMIN_PASS \
+  -opsmanageruser $OPSMANAGER_USER \
+  -destination . \
+  -tile ops-manager
+
+TIMESTAMP=$(date +"%Y%d%m-%H%M")
+
+tar -cf backup-output/opsmanager-$TIMESTAMP.tar opsmanager
